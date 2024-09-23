f6c8b71dffa724446a267558f9d49dc296da848b
KN0987 <minhkhangkg01@gmail.com>
2024-09-23 14:41:58 -0700

    Create sign in and sign up api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {errorHandler} from "../utils/error.js";
+
 
 export const signup = async (req,res, next) => {
     const {username, email, password} = req.body;
@@ -12,5 +15,22 @@ export const signup = async (req,res, next) => {
         next(err);
     }
 
-
 };
+
+export const signin = async (req,res,next) => {
+    const {username, password} = req.body;
+    try{
+        const validUser = await User.findOne({username});
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validUser || !validPassword) return next(errorHandler(401, "Username or password is not correct!"));
+        
+        //validUser._id is a unique id that mongodb automatically created for each user
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date((Date.now() + 3600000)*24); // 24 Hours
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+
+    }catch (error){
+        next(error);
+    }
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
 
 export default function SignUp() {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  
+  const [err,setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+  const handleChange = (event) => {
+    const {value, name} = event.target;
+    setInput(prev =>  {
+        return {
+            ...prev, 
+            [name]: value
+        };
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try{
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(input),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false){
+        setError(true);
+        return;
+      } 
+    }catch(error){
+      setLoading(false);
+      setError(true);
+    }
+    
+  }
   return (
-    <div>SignUp</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <form onSubmit={handleSubmit} className = "bg-[#F6F8FA] flex flex-col gap-4 justify-center items-center pt-12 pb-2 rounded-lg border-2 border-[#d1d9e0b3]">
+          <input type = "text" placeholder="Username"  name="username" className="w-3/4 p-3 rounded-lg border-2 border-[#d1d9e0b3]" onChange = {handleChange}/>
+          <input type = "text" placeholder="Email"  name="email" className="w-3/4 p-3 rounded-lg border-2 border-[#d1d9e0b3]" onChange = {handleChange}/>
+          <input type = "password" placeholder="Password" name="password" className="w-3/4 p-3 rounded-lg border-2 border-[#d1d9e0b3]" onChange = {handleChange}/>
+          <div className="flex justify-center">
+            <button disabled={loading} className="bg-slate-700 text-white rounded-lg w-40 h-8 hover:opacity-75 disabled:opacity-60">{loading ? 'Loading...':'Sign up'}</button>
+          </div>
+          <div className="flex mt-4 gap-2">
+          <p>Have an account?</p>
+          <Link to='/sign-in'>
+            <span className="text-blue-500">Sign in</span>
+          </Link>
+        </div>
+        <p className="text-red-700 mt-5">{err && "Username and email already exist!"}</p>
+        </form>
+
+    </div>
   )
 }
