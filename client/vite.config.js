485aa8b881a589daead5d2d00847d9bba9bbac53
KN0987 <minhkhangkg01@gmail.com>
2024-10-01 09:59:33 -0700

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 import dotenv from "dotenv"
 
 dotenv.config();
-// https://vitejs.dev/config/
+
 export default defineConfig({
   server: {
-    proxy:{
+    proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_BASE_URL  || "http://localhost:3000",
+        target:  process.env.VITE_BACKEND_BASE_URL || "http://localhost:3000",
         secure: false,
       },
-    }
+    },
+  },
+  build: {
+    outDir: 'dist',
   },
   plugins: [react()],
 })
