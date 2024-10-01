e2753cc25b94317e8c82b5fdab24d6690c965ebd
KN0987 <minhkhangkg01@gmail.com>
2024-10-01 11:50:20 -0700

    Debug

--- api/index.js
+++ api/index.js
@@ -19,7 +19,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "https://ai-summarizer-client.vercel.app",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
