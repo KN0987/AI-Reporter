8e069764e690ea21485899a4ed0a677c8a44b9a7
KN0987 <minhkhangkg01@gmail.com>
2024-10-01 08:24:31 -0700

    Update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
 mongoose
@@ -16,19 +15,11 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
-const __dirname = path.resolve();
-const corsOptions = {
-    origin: true, 
-    credentials: true, 
-};
+const port = process.env.PORT;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist','index.html'));
-});
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -46,8 +37,8 @@ app.use((err,req,res,next) => {
     })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server listening on port 3000")
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
 });
 
 
